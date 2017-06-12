45355a82b64549a2fb0330a30936118a2aea8a37
Debayan Singha <debayan@fusioncharts.com>
2017-06-12 15:00:48 +0530

    set up start sript in package.json and made adjustments for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.port || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -58,6 +59,6 @@ app.get('/bad', (req, res) => {
 	})
 });
 
-app.listen(3000, () => {
-	console.log('Server is up at port 3000');
+app.listen(port, () => {
+	console.log(`Server is up at port ${port}`);
 });
